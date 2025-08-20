1316c02316ec55f6239263253acaaa407d152ef4
Obiajuru Ekome <obiajuruekome@gmail.com>
2025-08-20 11:17:23 +0100

    Added new game modes to the code

--- RPS.py
+++ RPS.py
@@ -10,7 +10,7 @@ computer_score = 0
 
 def rock(): # When rock button is clicked
     global user_score, computer_score # Let's python know not to make new local variables for user score and compute score
-    page2.tkraise()
+    page3.tkraise()
     computer = random.choice(options)
     user = 'rock'
 
@@ -27,7 +27,7 @@ def rock(): # When rock button is clicked
 
 def paper(): #When paper is clicked
     global user_score, computer_score
-    page2.tkraise()
+    page3.tkraise()
     computer = random.choice(options)
     user = 'paper'
 
@@ -43,7 +43,7 @@ def paper(): #When paper is clicked
 
 def scissors(): #When scissors is clicked
     global user_score, computer_score
-    page2.tkraise()
+    page3.tkraise()
     computer = random.choice(options)
     user = 'scissors'
 
@@ -57,12 +57,59 @@ def scissors(): #When scissors is clicked
         computer_score += 1
     update_score()
 
+game_mode = None
+
 def update_score():
     Label_Score.config(text=f"You: {user_score} | Computer: {computer_score}")
 
+    global game_mode
+
+    if game_mode == "first5":
+        # Check if either player reached 5
+        if user_score == 5 and computer_score < 5:
+            page5.tkraise()
+            Label_Score2.config(text="PLAYER WINS", font=("Press Start 2P", 14))
+        elif computer_score == 5 and user_score < 5:
+            page5.tkraise()
+            Label_Score2.config(text="COMPUTER WINS", font=("Press Start 2P", 14))
+    elif game_mode == "first10":
+        if user_score == 10 and computer_score < 10:
+            page5.tkraise()
+            Label_Score2.config(text="PLAYER WINS", font=("Press Start 2P", 14))
+        elif computer_score == 10 and user_score < 10:
+            page5.tkraise()
+            Label_Score2.config(text="COMPUTER WINS", font=("Press Start 2P", 14))
+
+def unlimited():
+    global game_mode
+    game_mode = "unlimited"
+    page2.tkraise()
+    update_score()
+
+def first_to_five_click():
+    global game_mode
+    game_mode = "first5"
+    page2.tkraise()
+    update_score()
+
+def first_to_ten_click():
+    global game_mode
+    game_mode = "first10"
+    update_score()
+    page2.tkraise()
+
+
 def show_score():
     update_score()
-    page3.tkraise()
+    page4.tkraise()
+
+def restart_game():
+    global user_score, computer_score, game_mode
+    user_score = 0
+    computer_score = 0
+    game_mode = None
+    Label_Score.config(text="You: 0 | Computer: 0")
+    page1.tkraise()  # back to main menu
 
 
 root = tk.Tk()
@@ -75,31 +122,47 @@ container.pack(fill="both", expand=True)
 page1 = ttk.Frame(container)
 page2 = ttk.Frame(container)
 page3 = ttk.Frame(container)
+page4= ttk.Frame(container)
+page5 = ttk.Frame(container)
 
-for p in (page1, page2, page3):
+for p in (page1, page2, page3, page4, page5):
     p.place(relx=0, rely=0, relwidth=1, relheight=1) # stack pages full size
 
 def show_page1():
     page1.tkraise()
 
+def show_page2():
+    page2.tkraise()
+
 # Page 1
 ttk.Label(page1, text="ROCK, PAPER, SCISSORS", font=("Press Start 2P", 14)).pack(pady=10)
-ttk.Label(page1, text="Click an option to play", font=("Press Start 2P", 8)).pack(pady=10)
-ttk.Button(page1, text="Rock", command=rock).pack(pady=10)
-ttk.Button(page1, text="Paper", command=paper).pack(pady=10)
-ttk.Button(page1, text="Scissors", command=scissors).pack(pady=10)
-ttk.Button(page1, text="Show score", command=show_score).pack(pady=10)
+ttk.Label(page1, text="Which mode do you like to play", font=("Press Start 2P", 8)).pack(pady=10)
+ttk.Button(page1, text="Unlimited Mode", command=unlimited).pack(pady=10)
+ttk.Button(page1, text="First to 5", command=first_to_five_click).pack(pady=10)
+ttk.Button(page1, text="First to 10", command=first_to_ten_click).pack(pady=10)
 
-#Page 2
-Label_Result = ttk.Label(page2, text="")
-Label_Result.pack(pady=10)
-ttk.Button(page2, text="Play Again", command=show_page1).pack(pady=10)
+# Page 2
+ttk.Label(page2, text="ROCK, PAPER, SCISSORS", font=("Press Start 2P", 14)).pack(pady=10)
+ttk.Label(page2, text="Click an option to play", font=("Press Start 2P", 8)).pack(pady=10)
+ttk.Button(page2, text="Rock", command=rock).pack(pady=10)
+ttk.Button(page2, text="Paper", command=paper).pack(pady=10)
+ttk.Button(page2, text="Scissors", command=scissors).pack(pady=10)
+ttk.Button(page2, text="Show score", command=show_score).pack(pady=10)
 
 #Page 3
-Label_Score = ttk.Label(page3, text="You: 0 | Computer: 0", font=("Segoe UI", 14))
-Label_Score.pack(pady=10)
-ttk.Button(page3, text="Go back", command=show_page1).pack(pady=10)
+Label_Result = ttk.Label(page3, text="")
+Label_Result.pack(pady=10)
+ttk.Button(page3, text="Play Again", command=show_page2).pack(pady=10)
 
+#Page 4
+Label_Score = ttk.Label(page4, text="You: 0 | Computer: 0", font=("Segoe UI", 14))
+Label_Score.pack(pady=10)
+ttk.Button(page4, text="Go back", command=show_page2).pack(pady=10)
+
+# Page 5
+Label_Score2 = ttk.Label(page5, text="")
+Label_Score2.pack(pady=10)
+ttk.Button(page5, text="Restart Game", command=restart_game).pack(pady=10)
 
 show_page1()
 root.mainloop()
