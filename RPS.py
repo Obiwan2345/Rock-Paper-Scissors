768043907d02bee292b6b74347e6512d24318e1c
Obiajuru Ekome <obiajuruekome@gmail.com>
2025-08-21 11:56:00 +0100

    Added show score to page 5 and also added annotations

--- RPS.py
+++ RPS.py
@@ -9,7 +9,7 @@ user_score = 0
 computer_score = 0
 
 def rock(): # When rock button is clicked
-    global user_score, computer_score # Let's python know not to make new local variables for user score and compute score
+    global user_score, computer_score # Lets python know not to make new local variables for user score and compute score
     page3.tkraise()
     computer = random.choice(options)
     user = 'rock'
@@ -99,7 +99,7 @@ def first_to_ten_click():
     page2.tkraise()
 
 
-def show_score():
+def show_score(): # This function allows the user to see their score
     update_score()
     page4.tkraise()
 
@@ -134,7 +134,7 @@ def show_page1():
 def show_page2():
     page2.tkraise()
 
-# Page 1
+# Page 1, The opening page where the user picks the mode they want to play
 ttk.Label(page1, text="ROCK, PAPER, SCISSORS", font=("Press Start 2P", 14)).pack(pady=10)
 ttk.Label(page1, text="Which mode do you like to play", font=("Press Start 2P", 8)).pack(pady=10)
 ttk.Button(page1, text="Unlimited Mode", command=unlimited).pack(pady=10)
@@ -150,19 +150,21 @@ ttk.Button(page2, text="Scissors", command=scissors).pack(pady=10)
 ttk.Button(page2, text="Show score", command=show_score).pack(pady=10)
 
 #Page 3
-Label_Result = ttk.Label(page3, text="")
+Label_Result = ttk.Label(page3, text="") # Configured to show who won the round played
 Label_Result.pack(pady=10)
 ttk.Button(page3, text="Play Again", command=show_page2).pack(pady=10)
 
-#Page 4
+#Page 4 The score page
 Label_Score = ttk.Label(page4, text="You: 0 | Computer: 0", font=("Segoe UI", 14))
 Label_Score.pack(pady=10)
-ttk.Button(page4, text="Go back", command=show_page2).pack(pady=10)
+ttk.Button(page4, text="Go back", command=show_page2).pack(pady=10) # Allows to resume game
+ttk.Button(page4, text="Restart Game", command=restart_game).pack(pady=10) # Starts the game over entirely
 
 # Page 5
-Label_Score2 = ttk.Label(page5, text="")
+Label_Score2 = ttk.Label(page5, text="") # Configured to show who won the entire round only for first to 5 and first to 10
 Label_Score2.pack(pady=10)
 ttk.Button(page5, text="Restart Game", command=restart_game).pack(pady=10)
+ttk.Button(page5, text="Show score", command=show_score).pack(pady=10)
 
 show_page1()
 root.mainloop()
